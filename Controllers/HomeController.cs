f17ac223ed5c70da85f01865b54a4430c50a2689
ganeshpaiya <36655930+ganeshpaiya@users.noreply.github.com>
2018-06-13 18:03:29 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.text.html.simpleparser;
+
+
+namespace Export_PDF_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            return View(from customer in entities.Customers.Take(10)
+                        select customer);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export(string GridHtml)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                StringReader sr = new StringReader(GridHtml);
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf", "Grid.pdf");
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Export_PDF_MVC.MvcApplication" Language="C#" %>
